897a013de091ef7ad6e55fa236c5c4aea7e880c2
zl-max <582152734@qq.com>
2018-12-02 21:39:23 +0800

    隐藏未通过时下一步

--- public/install/js/install.js
+++ public/install/js/install.js
@@ -1,9 +1,8 @@
 $(document).ready(check_msg);
 // 检查是否通过环境验证
 function check_msg(){
-	var $href=$(".check_next").attr('href');
-	if($href==''){
-		$(".check_next").attr('disabled',true);
+	if($(".check_next").attr('href')==''){
+		$(".check_next").hide();
 	}
-	
+
 }
