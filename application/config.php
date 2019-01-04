acb81c28f5a2f8082fbfd795b110b8cf2c9f0b4a
zl-max <582152734@qq.com>
2019-01-05 00:09:55 +0800

    1

--- application/config.php
+++ application/config.php
@@ -23,7 +23,7 @@ return [
     // 是否支持多模块
     'app_multi_module'       => true,
     // 入口自动绑定模块
-    'auto_bind_module'       => false,
+    'auto_bind_module'       => true,
     // 注册的根命名空间
     'root_namespace'         => [],
     // 扩展函数文件
